a90cbda36c2edf0a62cf17e7048754036a563a09
HP4rr <fccf6422@gmail.com>
2025-12-18 21:03:56 -0800

    Added CPU, memory, and disk usage stats.
    Used psutil tools to list current CPU usage percentage, virtual memory usage percentage, as well as total storage, free storage, and used storage. Currently fixing the previous process list in order to show only the top 5 processes by CPU and memory usage instead of an insanely long and messy list of every single running process. (Used triple quotes around the old code block to prevent it from running for now)

--- siam.py
+++ siam.py
@@ -1,33 +1,41 @@
-#1/usr/bin/env python3
+
 """
 SIAM - System Integrity and Anomaly Monitor
-v0.1 - Initial live process list
+v0.2 - CPU, memory, and disk usage stats
 Author: [Carson Hale]
-Date: 2025-11-20
+Date: 2025-12-18
 """
 
 import psutil
-import os
-import time
+from psutil._common import bytes2human
 
-os.system('clear')
 
+# CPU Usage
+cpu_usage = psutil.cpu_percent(interval=1)
+print(f"CPU Usage: {cpu_usage} %")
+
+# Memory Usage
+memory_usage = psutil.virtual_memory()
+print (f"Memory Usage: {memory_usage.percent}%")
+
+# Disk Usage
+disk_usage = psutil.disk_usage('/')
+total_storage = disk_usage.total / (1024 ** 3)
+free_storage = disk_usage.free / (1024 ** 3)
+used_storage = disk_usage.used / (1024 ** 3)
+
+print (f"Total storage: {total_storage:.2f} GB")
+print (f"Free storage: {free_storage:.2f} GB")
+print (f"Storage used: {used_storage:.2f} GB")
+
+
+
+'''
 while True:
-    print ('SIAM v0.1 - Process Monitor. (0.2s refresh)')
+    print ("SIAM v0.1 - Process Monitor")
     print ('Press Cntrl+C to stop.\n')
-    
     for process in psutil.process_iter():
         processes = f"PID: {process.pid} | Name: {process.name()}"
         print (processes)
         time.sleep(.2)
-
-
-     
-        
-        
-        
-        
-        
-        
-    
-
+'''
